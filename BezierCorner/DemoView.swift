6e09ebdd7382e84b887dc758644c995be7cbf9fc
Andriy Trubchanin <trubchanin@river-soft.net>
2019-04-04 13:21:58 +0300

    custom corners

---
+++ BezierCorner/DemoView.swift
@@ -0,0 +1,96 @@
+//
+//  DemoView.swift
+//  BezierCorner
+//
+//  Created by Andriy Trubchanin on 4/4/19.
+//  Copyright © 2019 Andriy Trubchanin. All rights reserved.
+//
+
+import UIKit
+
+class DemoView: UIView
+{
+    var path: UIBezierPath!
+    
+    init(frame: CGRect, corners: [Corner], radius: CGFloat) {
+        super.init(frame: frame)
+        customCorner(radius: radius, corners: corners)
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+    }
+}
+
+extension DemoView
+{
+    enum Corner {
+        case topLeft
+        case topRight
+        case bottomRight
+        case bottomLeft
+    }
+    
+    func customCorner(radius: CGFloat = 0, corners: [Corner] = [])
+    {
+        path = UIBezierPath()
+        
+        if corners.contains(.topLeft) {
+            path.move(to: CGPoint(x: 0, y: radius))
+            path.addArc(withCenter: CGPoint(x: 0, y: 0),
+                        radius: radius,
+                        startAngle: CGFloat(90.0).toRadians(),
+                        endAngle: CGFloat(0.0).toRadians(),
+                        clockwise: false)
+        } else {
+            path.move(to: CGPoint(x: 0.0, y: 0.0))
+        }
+        
+        if corners.contains(.topRight) {
+            path.addLine(to: CGPoint(x: self.frame.size.width - radius, y: 0))
+            path.addArc(withCenter: CGPoint(x: self.frame.size.width, y: 0),
+                        radius: radius,
+                        startAngle: CGFloat(180.0).toRadians(),
+                        endAngle: CGFloat(90.0).toRadians(),
+                        clockwise: false)
+        } else {
+            path.addLine(to: CGPoint(x: self.frame.size.width, y: 0.0))
+        }
+
+        if corners.contains(.bottomRight) {
+            path.addLine(to: CGPoint(x: self.frame.size.width, y: self.frame.size.height - radius))
+            path.addArc(withCenter: CGPoint(x: self.frame.size.width, y: self.frame.size.height),
+                        radius: radius,
+                        startAngle: CGFloat(270.0).toRadians(),
+                        endAngle: CGFloat(180.0).toRadians(),
+                        clockwise: false)
+        } else {
+            path.addLine(to: CGPoint(x: self.frame.size.width, y: self.frame.size.height))
+        }
+
+        if corners.contains(.bottomLeft) {
+            path.addLine(to: CGPoint(x: radius, y: self.frame.size.height))
+            path.addArc(withCenter: CGPoint(x: 0.0, y: self.frame.size.height),
+                        radius: radius,
+                        startAngle: CGFloat(0.0).toRadians(),
+                        endAngle: CGFloat(270.0).toRadians(),
+                        clockwise: false)
+        } else {
+            path.addLine(to: CGPoint(x: 0.0, y: self.frame.size.height))
+        }
+
+        path.close()
+
+        let shapeLayer = CAShapeLayer()
+        shapeLayer.path = path.cgPath
+        
+        self.backgroundColor = UIColor.orange
+        self.layer.mask = shapeLayer
+    }
+}
+
+extension CGFloat {
+    func toRadians() -> CGFloat {
+        return self * CGFloat.pi / 180.0
+    }
+}
--- BezierCorner/ViewController.swift
+++ BezierCorner/ViewController.swift
@@ -8,13 +8,22 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController
+{
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        let width: CGFloat = 240.0
+        let height: CGFloat = 160.0
+        
+        let frame = CGRect(x: self.view.frame.size.width/2 - width/2,
+                           y: self.view.frame.size.height/2 - height/2,
+                           width: width,
+                           height: height)
+        
+        let demoView = DemoView(frame: frame, corners: [.topRight, .bottomRight], radius: 20)
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        self.view.addSubview(demoView)
     }
-
-
 }
 
