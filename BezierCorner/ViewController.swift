b3d53934ca16670a3fb9e56da690e65768d2d96f
Andriy Trubchanin <trubchanin@river-soft.net>
2019-04-04 11:07:54 +0300

    Initial Commit

---
+++ BezierCorner/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BezierCorner
+//
+//  Created by Andriy Trubchanin on 4/4/19.
+//  Copyright © 2019 Andriy Trubchanin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
